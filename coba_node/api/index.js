1e999bad2481446069049482734629c3fa632fb3
silvikusuma04 <silvikusuma043@gmail.com>
2026-02-24 13:47:40 +0700

    clean package.json for vercel

--- coba_node/api/index.js
+++ coba_node/api/index.js
@@ -2,12 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Request lewat jalur ini: ${req.url}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Halo ini Silvi Kusuma Wardhani Gunawan :)");
 });
 
-app.get("/say/:greeting", (req, res) => {
+app.get("/:greeting", (req, res) => {
   res.send(req.params.greeting);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
 
 export default app;

6ace389a98a85d6321a1275c49a48d8a82f30488
silvikusuma04 <silvikusuma043@gmail.com>
2026-02-24 14:41:17 +0700

    final

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+app.use((req, res, next) => {
+  if (false) {
+    next("not found");
+    return;
+  }
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.send("Error Occurred");
+});
+
+app.get("/", (req, res) => {
+  res.send("Halo ini Silvi Kusuma Wardhani Gunawan");
+});
+
+app.get("/:greeting", (req, res) => {
+  const { greeting } = req.params;
+  res.send(greeting);
+});
+ 
+app.listen(3000);
